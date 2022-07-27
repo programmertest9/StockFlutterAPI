3a7bd894d591d55ee16a827f507d433298c56e86
Tawatchai Hona <tawatchai.hona@ssup.co.th>
2022-07-27 22:23:04 +0700

    Stock Flutter API

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'stockflutterdb'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '1234'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
